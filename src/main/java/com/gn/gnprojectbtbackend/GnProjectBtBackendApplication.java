dfcd373a92a5ba570fe6f15cf1d9e46e4333e877
Diego Santivañez <Diegojsf17@gmail.com>
2024-06-26 17:53:53 -0500

    Initial commit

---
+++ src/main/java/com/gn/gnprojectbtbackend/GnProjectBtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gn.gnprojectbtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnProjectBtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GnProjectBtBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gn/gnprojectbtbackend/GnProjectBtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gn.gnprojectbtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnProjectBtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
